6f4ab492f98b21f388f99a460f07621507c2ff3f
tishkovets-maxim <tishkovets.maxim@outlook.com>
2023-07-01 02:11:59 +0800

    Минимально устраивающая меня диаграмма по п.1

---
+++ mpl_container_understanding.py
@@ -0,0 +1,50 @@
+﻿14:51 2023/6/29
+(предыдущее состояние файла похерило сбоем)
+
+Чтобы сделать подписи к БАРПЛОТУ, есть метод
+	Axes.bar_label(container, labels=None, *, fmt='%g', label_type='edge', padding=0, **kwargs)
+
+Чтобы его использовать, нужны:
+а) Axes - что очевидно
+б) объект container - что совершенно неочевидно		(что это такое - рассмотрю ниже)
+	ладно бы контейнер передавался опционально, пущей гибкости заради. 
+	Нет, он безальтернативно обязателен.
+Так и тянет написать для этого какую-нибудь обертку
+в) labels - массив меток. Опционален, но очевидно - если не передан, используются данные, ассоциированные с БАР-ами.
+	labels	array-like, optional
+	... If not given, the label texts will be the data values formatted with fmt.
+
+
+
+Кто такой контейнер
+https://matplotlib.org/stable/api/container_api.html
+Container for the artists of bar plots (e.g. created by Axes.bar).
+The container can be treated as a tuple of the patches themselves. Additionally, you can access these and further parameters by the attributes.
+
+Т.е. это такая прослойка между Axes и самими БАР-ами, включающая в себя сами БАР-ы и некоторые их свойства.
+
+У Axes есть атрибут containers - список отдельных "контейнеров".
+В случае простого Axes - контейнер в списке 1.
+	ax.containers[0].__dict__.keys()
+'patches', 'errorbar', 'datavalues', 'orientation', '_callbacks', '_remove_method', '_label', 'stale'
+	'patches': сами БАР-ы - объекты "прямоугольник" - matplotlib.patches.Rectangle
+		patches - родительский класс "прямоугольника" - "объект с двумя цветами - заливки и рамки"
+		Patches are Artists with a face color and an edge color.
+	'datavalues': массив (нампаевский) ассоциированных с ними значений
+	'_label': get_label()	Return the label used for this artist in the legend. В простом Axes - пустует. Посмотрю как у сложного.
+
+
+Таким образом, чтобы просто показать подписи у самого простого графика (пускай даже одного из группы), надо ввести такое:
+	sns_figure.axes[1].bar_label(sns_figure.axes[1].containers[0])
+
+Я кое-как научился побеждать вот эту вечную избыточность у пандов, и тут снова мне она.
+Оно еще и возвращает:	A list of Text instances for the labels.
+Т.е. его не встроить в цепочку.
+
+
+
+
+
+
+Вот тут относительно простые примеры, как с этим обращаться
+https://www.geeksforgeeks.org/how-to-show-values-on-seaborn-barplot/
---
+++ set_titles_understanding.py
@@ -0,0 +1,74 @@
+﻿# Создаем словарь args, куда записываем имена столбцов исходного датафрейма, соответствующие "строкам" и "столбцам" фасетгрида, под именами row_var, col_var
+args = dict(row_var=self._row_var, col_var=self._col_var)
+
+
+# Establish default templates
+# Если шаблона передано не было, задаем ШАБЛОН по умолчанию, в моем случае {col_var} = {col_name}
+
+# дальше делаем +- одно и то же, в двух вариантах в зависимости от определенности переменной self._margin_titles
+# я такую не заполнял, поэтому иду сразу во второй блок (хотя первый выглядит проще)
+
+
+# Проверяем заполненность "столбцов" и "строк"
+# Так они живут внутри объекта facetgrid:
+self._row_var		имя столбца датафрейма под "строки"
+self.row_names		уникальные значения этого столбца, они же имена "строк"
+
+self._col_var		имя столбца датафрейма под "столбцы"
+self.col_names		уникальные значения этого столбца, они же имена "столбцов"
+
+
+if (self._row_var is not None) and (self._col_var is not None):
+    ...
+elif self.row_names is not None and len(self.row_names):
+    ...
+# А вот и мой случай - когда есть только "столбцы"
+#	(проверяем, что переменная col_names а) задана, б) итерируется)
+#	А то мало ли, вдруг туда какую-то неитерируемую чушь занесло
+
+elif self.col_names is not None and len(self.col_names):
+
+    for i, col_name in enumerate(self.col_names):
+        args.update(dict(col_name=col_name))
+        title = template.format(**args)
+        # Index the flat array so col_wrap works
+        self.axes.flat[i].set_title(title, **kwargs)
+
+
+Перебираем col_names, так что на каждой итерации col_name - текущий элемент, i - номер текущего элемента
+1. Записываем текущий элемент в args под именем "col_name"
+2. Формируем строку title, так что template заполняется знакоместами из args
+	а там у нас - только col_var и col_name
+	под что-то другое места НЕ предусмотрено.
+	это НЕ f-string, это Format Specification Mini-Language
+3. Назначаем строку title заголовком соответствующего Axes
+	(причем Axes) сплющиваем, "чтобы работал col_wrap"
+	self.axes.flat
+flat - СВОЙСТВО нампаевского массива, возвращающее flatiter - специальный итератор, перебирающий конечные массива, представленного в "плоском" (одномерном) виде
+
+
+В моем случае - это действие избыточно
+		sns_figure.axes.__class__   # numpy.ndarray
+		sns_figure.axes.shape       # (6,)
+Я не знаю, было, будет или в иных условиях возможна другая размерность у axes, но логика ясна
+
+
+Отсюда же мы узнаем еще один факт: у Facetgrid нету какой-то определенной специальной сущности, обволакивающей отдельные графики.
+Есть инкорпорированная Figure, есть "ярлык" на Axes этой Figure, есть всякие собственные объекты вроде col_names
+Но общей сущности в которой вместе были бы axes.flat[i] и col_names[i], чтобы можно было сделать 
+так:
+for facet in sns_figure.facets:
+	facet.set_title (f"{facet.col_name} чего-то там {какая-нибудь еще переменная}")
+нет
+
+
+
+19:50 2023/6/28
+for (i, ax) in enumerate(sns_figure.axes.flat):
+    print (ax.title.__class__)
+
+<class 'matplotlib.text.Text'>
+...
+
+Т.е. title - поле не просто string, а именно матплотлибовский Text, у которого есть собственно текстовая составляющая, плюс координаты (и надо полагать, что-то еще)
+
